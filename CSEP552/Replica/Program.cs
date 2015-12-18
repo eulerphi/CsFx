9ba9d1fc54d26a17e81e58d97cb3dc888dcca3af
eulerphi <jacowhig@gmail.com>
2015-12-18 07:03:57 -0800

    Add projects for replica and replica tester. Decided to roll my own rpc on top of tcp.

---
+++ CSEP552/Replica/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ep = new IPEndPoint(IPAddress.Loopback, 1444);
+            var listener = new TcpListener(ep);
+
+            listener.Start();
+            while(true)
+            {
+                listener.AcceptTcpClientAsync()
+                        .ContinueWith(Handle, TaskContinuationOptions.OnlyOnRanToCompletion);
+            }
+        }
+
+        private static void Handle(Task<TcpClient> openClientTask)
+        {
+            var buffer = new byte[1024];
+
+            var stream = openClientTask.Result.GetStream();
+            var offset = 0;
+            while (stream.DataAvailable)
+            {
+                var n = stream.Read(buffer, offset, buffer.Length);
+                offset += n;
+
+                var message = Encoding.ASCII.GetString(buffer, 0, n);
+                Console.WriteLine(message);
+            }
+
+        }
+    }
+}
---
+++ CSEP552/RpcClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ep = new IPEndPoint(IPAddress.Loopback, 1444);
+            var client = new TcpClient();
+            client.Connect(ep);
+            var stream = client.GetStream();
+
+            var message = Encoding.ASCII.GetBytes("Hello, World!");
+            var message2 = Encoding.ASCII.GetBytes("This is an another message!");
+
+            stream.Write(message, 0, message.Length);
+            stream.Write(message2, 0, message2.Length);
+
+            Console.Read();
+        }
+    }
+}
